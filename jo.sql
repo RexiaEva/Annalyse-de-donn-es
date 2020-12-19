22ed1c9260f98f822ea64e6d93bd056388216120
Amaury Bonneau <amaury.bonneau@isen-ouest.yncrea.fr>
2020-12-19 19:46:44 +0100

    la dernière base en date, j'ai passé la colonne an dans jeux en int car je sais pas comment modifier la donnée envoyée par le curseur python pour qu'elle soit adaptée au type YEAR)

--- jo.sql
+++ jo.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.4
+-- version 4.9.5
 -- https://www.phpmyadmin.net/
 --
--- Client :  127.0.0.1
--- Généré le :  Mar 15 Décembre 2020 à 16:51
--- Version du serveur :  5.7.14
--- Version de PHP :  7.0.10
+-- Host: localhost:8081
+-- Generation Time: Dec 19, 2020 at 06:40 PM
+-- Server version: 5.7.24
+-- PHP Version: 7.4.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -17,161 +19,256 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données :  `jo`
+-- Database: `jo`
 --
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `athletes`
+-- Table structure for table `athletes`
 --
 
 CREATE TABLE `athletes` (
-  `id` int(8) UNSIGNED NOT NULL,
+  `id_athlete` int(11) NOT NULL,
   `nom` varchar(255) NOT NULL,
-  `sexe` char(1) NOT NULL,
-  `age` tinyint(3) UNSIGNED NOT NULL,
-  `taille` tinyint(3) UNSIGNED DEFAULT NULL,
-  `poids` tinyint(3) UNSIGNED DEFAULT NULL,
-  `equipe` varchar(255) NOT NULL,
-  `NOC_id` int(10) UNSIGNED NOT NULL
+  `sexe` char(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `ath_epr`
+-- Table structure for table `athletes_epreuves`
 --
 
-CREATE TABLE `ath_epr` (
-  `id` int(11) UNSIGNED NOT NULL,
-  `ath_id` int(8) UNSIGNED NOT NULL,
-  `epr_id` int(10) UNSIGNED NOT NULL,
-  `medaille` varchar(26) NOT NULL
+CREATE TABLE `athletes_epreuves` (
+  `id_athlete` int(11) NOT NULL,
+  `id_epreuve` int(11) NOT NULL,
+  `medaille` varchar(26) NOT NULL,
+  `age` int(11) NOT NULL,
+  `poids` int(11) NOT NULL,
+  `taille` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `epreuves`
+-- Table structure for table `athletes_sports`
+--
+
+CREATE TABLE `athletes_sports` (
+  `id_athlete` int(11) NOT NULL,
+  `id_sport` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `epreuves`
 --
 
 CREATE TABLE `epreuves` (
-  `id` int(11) UNSIGNED NOT NULL,
-  `sport` varchar(26) NOT NULL,
-  `evenement` varchar(26) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+  `id_epreuve` int(11) NOT NULL,
+  `id_sport` int(11) NOT NULL,
+  `epreuve` varchar(26) NOT NULL,
+  `id_jeu` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `jeux`
+-- Table structure for table `equipes`
+--
+
+CREATE TABLE `equipes` (
+  `id_equipe` int(11) NOT NULL,
+  `nom` varchar(255) NOT NULL,
+  `id_noc` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `equipes_athletes`
+--
+
+CREATE TABLE `equipes_athletes` (
+  `id_equipe` int(11) NOT NULL,
+  `id_athlete` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jeux`
 --
 
 CREATE TABLE `jeux` (
-  `id` int(11) UNSIGNED NOT NULL,
-  `jeu` varchar(26) NOT NULL,
-  `an` year(4) NOT NULL,
-  `saison` varchar(26) NOT NULL,
-  `ville` varchar(26) NOT NULL
+  `id_jeu` int(11) NOT NULL,
+  `jeu` varchar(255) NOT NULL,
+  `annee` int(11) NOT NULL,
+  `saison` varchar(255) NOT NULL,
+  `ville` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `nations`
+-- Table structure for table `nocs`
 --
 
-CREATE TABLE `nations` (
-  `id` int(11) UNSIGNED NOT NULL,
+CREATE TABLE `nocs` (
+  `id_noc` int(11) NOT NULL,
   `NOC` char(3) NOT NULL,
-  `region` varchar(26) NOT NULL,
+  `region` varchar(255) NOT NULL,
   `notes` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+-- --------------------------------------------------------
+
 --
--- Index pour les tables exportées
+-- Table structure for table `sports`
+--
+
+CREATE TABLE `sports` (
+  `id_sport` int(11) NOT NULL,
+  `sport` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
 --
 
 --
--- Index pour la table `athletes`
+-- Indexes for table `athletes`
 --
 ALTER TABLE `athletes`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `nom` (`nom`),
-  ADD KEY `fk_nation_ath` (`NOC_id`);
+  ADD PRIMARY KEY (`id_athlete`);
 
 --
--- Index pour la table `ath_epr`
+-- Indexes for table `athletes_epreuves`
 --
-ALTER TABLE `ath_epr`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_ae_ath` (`ath_id`),
-  ADD KEY `epr_id` (`epr_id`);
+ALTER TABLE `athletes_epreuves`
+  ADD KEY `id_athlete` (`id_athlete`),
+  ADD KEY `id_epreuve` (`id_epreuve`);
 
 --
--- Index pour la table `epreuves`
+-- Indexes for table `athletes_sports`
+--
+ALTER TABLE `athletes_sports`
+  ADD KEY `id_sport` (`id_sport`),
+  ADD KEY `id_athlete` (`id_athlete`);
+
+--
+-- Indexes for table `epreuves`
 --
 ALTER TABLE `epreuves`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id_epreuve`),
+  ADD KEY `id_jeu` (`id_jeu`),
+  ADD KEY `id_sport` (`id_sport`);
 
 --
--- Index pour la table `jeux`
+-- Indexes for table `equipes`
+--
+ALTER TABLE `equipes`
+  ADD PRIMARY KEY (`id_equipe`);
+
+--
+-- Indexes for table `equipes_athletes`
+--
+ALTER TABLE `equipes_athletes`
+  ADD KEY `id_equipe` (`id_equipe`),
+  ADD KEY `id_athlete` (`id_athlete`);
+
+--
+-- Indexes for table `jeux`
 --
 ALTER TABLE `jeux`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id_jeu`);
 
 --
--- Index pour la table `nations`
+-- Indexes for table `nocs`
 --
-ALTER TABLE `nations`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `NOC` (`NOC`);
+ALTER TABLE `nocs`
+  ADD PRIMARY KEY (`id_noc`);
 
 --
--- AUTO_INCREMENT pour les tables exportées
+-- Indexes for table `sports`
+--
+ALTER TABLE `sports`
+  ADD PRIMARY KEY (`id_sport`);
+
+--
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT pour la table `athletes`
+-- AUTO_INCREMENT for table `athletes`
 --
 ALTER TABLE `athletes`
-  MODIFY `id` int(8) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id_athlete` int(11) NOT NULL AUTO_INCREMENT;
+
 --
--- AUTO_INCREMENT pour la table `ath_epr`
---
-ALTER TABLE `ath_epr`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT pour la table `epreuves`
+-- AUTO_INCREMENT for table `epreuves`
 --
 ALTER TABLE `epreuves`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id_epreuve` int(11) NOT NULL AUTO_INCREMENT;
+
 --
--- AUTO_INCREMENT pour la table `jeux`
+-- AUTO_INCREMENT for table `equipes`
+--
+ALTER TABLE `equipes`
+  MODIFY `id_equipe` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `jeux`
 --
 ALTER TABLE `jeux`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id_jeu` int(11) NOT NULL AUTO_INCREMENT;
+
 --
--- AUTO_INCREMENT pour la table `nations`
+-- AUTO_INCREMENT for table `nocs`
 --
-ALTER TABLE `nations`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+ALTER TABLE `nocs`
+  MODIFY `id_noc` int(11) NOT NULL AUTO_INCREMENT;
+
 --
--- Contraintes pour les tables exportées
+-- AUTO_INCREMENT for table `sports`
+--
+ALTER TABLE `sports`
+  MODIFY `id_sport` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
 --
 
 --
--- Contraintes pour la table `athletes`
+-- Constraints for table `athletes_epreuves`
 --
-ALTER TABLE `athletes`
-  ADD CONSTRAINT `fk_nation_ath` FOREIGN KEY (`NOC_id`) REFERENCES `nations` (`id`);
+ALTER TABLE `athletes_epreuves`
+  ADD CONSTRAINT `athletes_epreuves_ibfk_1` FOREIGN KEY (`id_athlete`) REFERENCES `athletes` (`id_athlete`),
+  ADD CONSTRAINT `athletes_epreuves_ibfk_2` FOREIGN KEY (`id_epreuve`) REFERENCES `epreuves` (`id_epreuve`);
 
 --
--- Contraintes pour la table `ath_epr`
+-- Constraints for table `athletes_sports`
 --
-ALTER TABLE `ath_epr`
-  ADD CONSTRAINT `fk_ae_ath` FOREIGN KEY (`ath_id`) REFERENCES `athletes` (`id`);
+ALTER TABLE `athletes_sports`
+  ADD CONSTRAINT `athletes_sports_ibfk_1` FOREIGN KEY (`id_sport`) REFERENCES `sports` (`id_sport`),
+  ADD CONSTRAINT `athletes_sports_ibfk_2` FOREIGN KEY (`id_athlete`) REFERENCES `athletes` (`id_athlete`);
+
+--
+-- Constraints for table `epreuves`
+--
+ALTER TABLE `epreuves`
+  ADD CONSTRAINT `epreuves_ibfk_1` FOREIGN KEY (`id_jeu`) REFERENCES `jeux` (`id_jeu`),
+  ADD CONSTRAINT `epreuves_ibfk_2` FOREIGN KEY (`id_sport`) REFERENCES `sports` (`id_sport`);
+
+--
+-- Constraints for table `equipes_athletes`
+--
+ALTER TABLE `equipes_athletes`
+  ADD CONSTRAINT `equipes_athletes_ibfk_1` FOREIGN KEY (`id_equipe`) REFERENCES `equipes` (`id_equipe`),
+  ADD CONSTRAINT `equipes_athletes_ibfk_2` FOREIGN KEY (`id_athlete`) REFERENCES `athletes` (`id_athlete`);
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
